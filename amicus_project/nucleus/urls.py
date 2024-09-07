540405f17015df51c41f8eb4dadb0de5c9149109
scottsantinho <scottsantinho@yahoo.fr>
2024-09-07 21:08:31 +0200

    final commit 7th Sept

--- amicus_project/nucleus/urls.py
+++ amicus_project/nucleus/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='nucleus/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='home', template_name='nucleus/home.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.update_profile, name='update_profile'),
 ]
