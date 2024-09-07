2feb0860a6d4eb2c098b69cb58b3e17a6d15e6f9
scottsantinho <scottsantinho@yahoo.fr>
2024-09-07 20:37:40 +0200

    Minor changes for the login/logout

--- amicus_project/nucleus/urls.py
+++ amicus_project/nucleus/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.update_profile, name='update_profile'),
     path('', views.home, name='home'),
+    # Add these new paths
+    path('login/', auth_views.LoginView.as_view(template_name='nucleus/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
